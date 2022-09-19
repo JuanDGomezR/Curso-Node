0460ee4d5730ded9194ed9b9e9c24f638b4f35a3
Juan <juandavidgomezruidiaz2004@gmail.com>
2022-09-19 01:09:38 -0500

    curso node js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routerApi = require('./routes');
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('hola, soy una nueva ruta');
+});
+
+
+routerApi(app)
+
+
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
